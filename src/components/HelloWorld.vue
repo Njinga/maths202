f632661112e2eb0336723a7540e31685c834ad44
Njinga Nkosi <47808032+Njinga@users.noreply.github.com>
2020-06-26 23:36:43 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,130 @@
+<template>
+    <mdb-container>
+        <!-- -->
+        <section class="text-center white-text special-color  py-3 px-3">
+            <h4 class="pt-2 pb-2 text-center"> UNIT 1: Multi-variable functions</h4>
+        </section>
+        <section class="text-center dark-grey-text mb-4 mt-4">
+            <h4 class="font-weight-bold pb-4 text-center">
+                <mdb-icon icon="marker" size="lg" class="mr-3" />
+                Learning outcomes
+                <mdb-icon icon="graduation-cap" size="lg" class="ml-3" />
+            </h4>
+            <mdb-row class="text-left">
+                <!-- Choose appropriate mdb-col  -->
+                <mdb-col class="text-left w-responsive mx-auto m-3">
+                    <p>
+                        <ul>
+                            <li>
+                                <strong><em>Identify</em></strong> and <strong><em>sketch</em></strong> the domain and range of
+                                functions of two variables.
+                            </li>
+                            <li><strong><em>Derive</em></strong> revenue and profit functions from given cost data and demand schedule.</li>
+                            <li>
+                                <strong><em>Define</em></strong> and <strong><em>compute</em></strong> the first and second order partial derivatives of functions
+                                of two or more variables.
+                            </li>
+                            <li>
+                                <strong><em>Use</em></strong> partial derivatives to <strong><em>calculate</em></strong>
+                                marginal quantities, such as cost, profit,
+                                revenue, productivity and utility, and interpret the results.
+                            </li>
+                            <li>
+                                <strong><em>Use</em></strong> partial derivatives to <strong><em>determine</em></strong>
+                                whether products are complementary or substitutes.
+                            </li>
+                            <li>
+                                <strong><em>Construct</em></strong> and <strong><em>solve</em></strong> unconstrained optimization problems, defined as functions of two
+                                variables, using the second partial derivative test.
+                            </li>
+                            <li>
+                                <strong><em>Formulate</em></strong> and <strong><em>solve</em></strong> constrained optimization problems using the substitution method
+                                and the method of Lagrange Multipliers.
+                            </li>
+                            <li>
+                                <strong><em>Use</em></strong> partial derivatives to <strong><em>solve</em></strong> a class of optimization problems with applications in
+                                business and economics. Examples to include
+                            
+                            <ul>
+                                <li>	Minimising costs subject to production quotas.</li>
+                                <li>	Maximising utility subject to budgetary constraints</li>
+                                <li> Maximising production subject to a cost constraint. </li>
+                            </ul>
+                                </li>
+                        </ul>
+                    </p>
+                </mdb-col>
+            </mdb-row>
+        </section>
+        <hr />
+
+        <section class="text-center dark-grey-text mb-4 mt-4">
+            <h4 class="font-weight-bold pb-4 text-center">Introduction</h4>
+            <mdb-row>
+                <!-- Choose appropriate mdb-col  -->
+                <mdb-col>
+                </mdb-col>
+            </mdb-row>
+        </section>
+        <hr />
+        <section class="text-center dark-grey-text mb-4">
+            <h4 class="font-weight-bold pb-4 text-center"> Partial Derivatives</h4>
+            <mdb-row>
+                <!-- Choose appropriate mdb-col  -->
+                <mdb-col>
+                    <strong>Application of partial derivatives</strong>
+                </mdb-col>
+            </mdb-row>
+        </section>
+        <hr />
+        <section class="text-center dark-grey-text mb-4">
+            <h4 class="font-weight-bold pb-4 text-center">
+                Unconstrained Optimization
+            </h4>
+            <mdb-row>
+                <!-- Choose appropriate mdb-col  -->
+                <mdb-col>
+                </mdb-col>
+            </mdb-row>
+        </section>
+        <hr />
+        <section class="text-center dark-grey-text mb-4">
+            <h4 class="font-weight-bold pb-4 text-center">
+                Constrained Optimization
+            </h4>
+            <mdb-row>
+                <!-- Choose appropriate mdb-col  -->
+                <mdb-col>
+                </mdb-col>
+            </mdb-row>
+        </section>
+        <hr />
+        <section class="text-center dark-grey-text mb-4">
+            <h4 class="font-weight-bold pb-4 text-center">Summary</h4>
+            <mdb-row>
+                <!-- Choose appropriate mdb-col  -->
+                <mdb-col>
+                </mdb-col>
+            </mdb-row>
+        </section>
+    </mdb-container>
+</template>
+<script>
+    import { mdbContainer, mdbRow, mdbCol, mdbIcon } from "mdbvue";
+    export default {
+        name: "Unit1",
+        components: {
+            mdbContainer,
+            mdbRow,
+            mdbCol,
+            mdbIcon
+        },
+        data() {
+            return {
+            };
+        },
+    }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
