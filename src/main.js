8a381ce9a00d78f802918f2bb9f99e0d0c91fae0
Njinga Nkosi <47808032+Njinga@users.noreply.github.com>
2020-06-27 19:23:36 +0200

    done with application of derivatives
    Cobb_Douglass_Marginal_Productivity.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/lib/css/mdb.min.css'
 
 import Vue from 'vue'
+import katex from 'vue-katex'
+import 'katex/dist/katex.min.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(katex)
 
 import Notify from 'mdbvue/lib/components/Notify'
 Vue.use(Notify)
