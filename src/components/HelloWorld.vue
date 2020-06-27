20e96a6901aa9d1f9d5ff9192d72f183f399b82a
Khuks <khulekanimkholo30@gmail.com>
2020-06-27 18:34:40 +0100

    Updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -92,8 +92,144 @@
             <h4 class="font-weight-bold pb-4 text-center">
                 Constrained Optimization
             </h4>
+
             <mdb-row>
                 <!-- Choose appropriate mdb-col  -->
+                <!--Lagrange Multipliers  -->
+                <mdb-col class="text-left w-responsive mx-auto m-3">
+                    
+                    <h5 class="pb-4 font-weight-bold red-text text-center">Lagrange Multipliers</h5>
+                    <h6 class="text-left">Procedure for method of Lagrange Multipliers</h6>
+                    <ol>
+                    <li>
+                        <p> Formulate the problem in the form</p>
+                        <p>Maximize (or Minimize) </p><span v-katex="'\ z = f{(x,y)}'"></span>
+                        <br>
+                        <p>Subject to </p><span v-katex="'\ g{(x,y)} = 0'"></span>
+                        <br>
+                    </li>
+                    <li>
+                        Introduce the Lagrange multiplier <span v-katex="'\\lambda'"></span>, and construct the Lagrangean
+                        function
+                                        <div v-katex:display="'\ F{(x,y)} = f(x,y) +  = \\lambda g{(x,y)}  '"></div>
+                        </li>
+                        <li>
+                            <p>Optimize the Lagrangean by solving</p>
+                        <div v-katex:display="'\ F_x = f_x + \\lambda g_x = 0'"></div>
+                        <div v-katex:display="'\ F_y = f_y + \\lambda , g_y = 0'"></div>
+                        <div v-katex:display="'\ F_\\lambda = g(x,y) = 0'"></div>
+                        <div>Fx = fx +\gx=0</div>
+                        <div>Fy = fx +\,gy=0</div>
+                        <div>F\ = g(x,y)=0</div>
+                        <br>
+                        <p>simultaneously for x, y and \ to get the critical point <span v-katex="'\ x_0 , y_0 \\lambda_0'"></span></p>
+                        </li>
+                        <li>
+                        Evaluate the optimal value <span v-katex="'\ f(x_0,y_0)'"></span>
+
+
+                        </li>
+                    </ol>
+                        <h6 class="text-left">Utility and Utility Maximisation</h6>
+                    
+                    <div>
+                    <p class="indigo-text">What is Utility</p>
+                    <p>
+                        Pleasure or satisfaction (value for money) derived by a person from the
+                        consumption of a good or service.
+                    </p>
+                    </div>
+
+                    <div>
+                    <p class="indigo-text">What is Utility?</p>
+                    <p>
+                        Economics concept that, when making a purchase decision, a consumer
+                        attempts to get the greatest value possible from expenditure of least
+                        amount of money. His or her objective is to maximize the total value
+                        derived from the available money.
+                    </p>
+                    <p>Read more: <a href="http://www.businessdictionary.com">http://www.businessdictionary.com</a></p>
+
+                    </div>
+
+                    <div>
+                    <p class="green-text text-left">Example (1)</p>
+                    <p>Suppose that a consumer has the utility function</p>
+                        <div v-katex="'\ f(x,y)=x^{4}y^{2} '"></div>
+                        <br>
+                        <p>
+                            where x is the number of units of commodity A and y is the number of
+                            units of commodity B. Suppose further that the consumer has E18 to
+                            spend. If a unit of the commodity A costs E3 and a unit of B costs E1,
+                            use the method of Lagrange multipliers to maximise the utility.
+                        </p>
+                    </div>
+
+                        <h6 class="text-left">Utility and Utility Maximisation</h6>
+
+                        <p><span class="red-text">Solution:</span><br> We start by formulating the constraint equation <span v-katex="'\ g(x,y)'"></span>  
+                            from the budget equation budget equation.
+                        </p>
+                        <p>
+                            The total cost is <span v-katex="'\ 3x + y '"></span>. Thus, the budget equation is <span v-katex="'\ 3x + y = 18'"></span> .
+                            We define the constraint equation as
+                        </p>
+                        <div v-katex="'\ g(x,y) = 3x + y -18'"></div>
+                        <p>
+                            We then form the langrangian fuction with the Langrange multiplier <span v-katex="'\\lambda '"></span>
+                        </p>
+
+                        <div v-katex="'\ F(x,y, \\lambda) = f(x,y) + \\lambda g(x,y)'"></div>
+                        <div v-katex="'\ F(x,y, \\lambda) = x^{4}y^{2} + \\lambda (3x + y -18)'"></div>
+
+
+                        <h6 class="text-left">Optimising the Lagrangean</h6>
+                        <p>
+                            To optimize the Lagrangean we obtain critical points by solving the system
+                            of equations that result from equating the first order partial derivatives to
+                            zero.
+                        </p>
+                        <p>The partial derivatives are</p>
+                        <div v-katex="'\ F_x = 4x^{3}y^{2} + 3\\lambda = 0'"></div>
+                        <div v-katex="'\ F_y = 2x^{4}y + \\lambda = 0 '"></div>
+                        <div v-katex="'\ F_\\lambda = 3x + y - 18  = 0 '"></div>
+
+                        <p>Thus, we must solve</p>
+
+                        <div v-katex="'\ 4x^{3} + 3\\lambda  = 0 '"></div>
+                        <div v-katex="'\ 2x^{4}y + \\lambda  = 0 '"></div>
+                        <div v-katex="'\ 3x + y - 18  = 0 '"></div>
+
+                        <h6 class="text-left">Solving for <span v-katex="'\ x , y \\textrm{ and } \\lambda'"></span></h6>
+                        <p>Making <span v-katex="'\\lambda'"></span> the subject gives</p>
+                        <div v-katex="'\\lambda = -\\frac{4}{3} x^{3}y^{2} \\textrm{ and }  -2x^{4}y '"></div>
+
+                        <p>Equating the two equations gives</p>
+                        <div v-katex="'\-\\frac{4}{3} x^{3}y^{2} = -2x^{4}y '"></div>
+                        <div v-katex="'\\lambda = -\ 4x^{3}y^{2} = -6x^{4}y '"></div>
+                        <div v-katex="'\ 2y = 3x '"></div>
+
+                        <h6 class="text-left">Evaluating the optimal point</h6>
+                        <p>Having found the equation <span v-katex="'\ 2y = 3x'"></span> we must solve the pair of equations</p>
+                        <div v-katex="'\ 2y = 3x \\textrm{ and }  3x + y = 18'"></div>
+                        <p>From the second equation we have, <span v-katex="'\ y = 18-3x'"></span></p>
+                        <p>Substituting in <span v-katex="'\ 2y =  3x'"></span> gives</p>
+
+                        <div v-katex="'\ 2(18-3x) = 3x '"></div>
+                        <div v-katex="'\ 36 - 6x = 3x = 3x '"></div>
+                        <div v-katex="'\ 36 = 9x '"></div>
+
+                        <p>This implies that  <span v-katex="'\ x=4  \\textrm{ and }   y=18-3(4) = 6'"></span></p>
+                        <p>Since the solution (4,6) <span v-katex="'\ (4,6)'"></span> is the only critical point, we conclude that the
+                            maximum utility is</p>
+
+                        <div v-katex="'\ f(4,6) = (4)^{4}(6)^{2} = 9216'"></div>
+                            
+
+                </mdb-col> 
+        <!--Lagrange Multipliers  -->
+
+
                 <mdb-col>
                 </mdb-col>
             </mdb-row>
@@ -111,6 +247,7 @@
 </template>
 <script>
     import { mdbContainer, mdbRow, mdbCol, mdbIcon } from "mdbvue";
+    import 'katex/dist/katex.min.css';
     export default {
         name: "Unit1",
         components: {
--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/lib/css/mdb.min.css'
+import 'katex/dist/katex.min.css';
 
+import VueKatex from 'vue-katex';
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Notify from 'mdbvue/lib/components/Notify'
+
 
 Vue.config.productionTip = false
-
-import Notify from 'mdbvue/lib/components/Notify'
 Vue.use(Notify)
+Vue.use(VueKatex, {
+  globalOptions: {
+   
+  }
+});
 
 new Vue({
   router,
