38c60ae1662279472b3291c7cff882633321879c
patiesoft <patiesoft@gmail.com>
2020-06-27 04:23:42 +0200

    Done First Page of Classifying Critical Points

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -84,6 +84,39 @@
             <mdb-row>
                 <!-- Choose appropriate mdb-col  -->
                 <mdb-col>
+                    <!--Classifying Critical Points-->
+                    <div>
+                        <!--Headings-->
+                        <strong class="text-danger">Classifying extreme points</strong>
+                        <h5 class="font-weight-bold pb-2 pt-2">MS202 </h5>
+                        <p class="font-weight-bold pb-3">Examples on functions of two variables <span v-katex="'f(x,y)'"></span></p>
+                        <h6 class="font-weight-bold pb-1 headingUnd">SECOND DERIVATIVE TEST FOR LOCAL EXTREMA </h6> 
+                        <!--/Headings-->
+
+                        <!--Theorem--> 
+                        <h6 class="text-primary text-left font-weight-bold">Theorem</h6>
+                        <blockquote class="blockquote">
+                            <div class="z-depth-1 p-2 pl-4"> 
+                                <p class="text-left">
+                                    <span v-katex="'\\textrm{Suppose that }'"></span> <span v-katex="'f(x,y)'"></span> <span v-katex="'\\textrm{ and its first and second partial derivatives are continuous in the domain of }'"></span>
+                                    <span v-katex="'f'"></span> <span v-katex="'\\textrm{ that, at the critical points }'"></span> <span v-katex='"(x,y) = (a,b)"'></span> <span v-katex="'\\textrm{ we have }'"></span> <span v-katex:display="'f_x = f_y = 0'"></span>.
+                                    <ol type="1">
+                                        <li class="mb-2 pl-2"> <span v-katex="'\\textrm{Let } D = f_{xx}f_{yy} - f_{xy}^2 \\textrm{ at } (a,b) \\textrm{, then}'"></span></li> 
+                                        <li class="mb-2 pl-2"> <span v-katex="'\\textrm{if } D > 0 \\textrm{ and } f_{xx} < 0 \\textrm{ at } (a,b), \\textrm{ f has a local maximum at } (a,b).'"></span></li>
+                                        <li class="mb-2 pl-2"> <span v-katex="'\\textrm{if } D < 0 \\textrm{ at } (a,b), f \\textrm{ has a saddle point at } (a,b).'"></span> </li>   
+                                        <li class="mb-2 pl-2"> <span v-katex="'\\textrm{if } D=0, \\textrm{ the test is inconclusive and } f \\textrm{ may either have a relative extremum or a saddle point at} (a,b)\\textrm{.}'"></span> </li>
+                                        <li class="mb-2 pl-2"> <span v-katex="'\\textrm{if } D > 0 \\textrm{ and } f_{xx} > 0 \\textrm{ at } (a,b), f \\textrm{ has a local minimum at (a,b).}'"></span></li>
+                                        <li class="pl-2"> <span v-katex="'\\textrm{if } D > 0 \\textrm{ and } f_{xx} > 0 \\textrm{ at } (a,b), \\textrm{ f has a local minimum at} (a,b).'"></span> </li> 
+                                    </ol> 
+                                </p>
+                            </div> 
+                        </blockquote>
+                        <!--/Theorem--> 
+                        <P>
+                            <span v-katex="'\\textrm{The expression } D = f_{xx}f_{yy} - f_{xy}^2 \\textrm{ is known as the} \\textbf{ discriminant} \\textrm{ of f}'"></span>
+                        </P> 
+                    </div>
+                    <!--/Classifying Critical Points--> 
                 </mdb-col>
             </mdb-row>
         </section>
@@ -111,6 +144,7 @@
 </template>
 <script>
     import { mdbContainer, mdbRow, mdbCol, mdbIcon } from "mdbvue";
+    import 'katex/dist/katex.min.css';
     export default {
         name: "Unit1",
         components: {
@@ -127,4 +161,15 @@
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.headingUnd{
+    border-bottom: 2px solid currentColor;
+    display: inline-block;
+    line-height: 0.85;
+    text-shadow:
+    2px 2px white,
+    2px -2px white,
+    -2px 2px white,
+    -2px -2px white;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueKatex from 'vue-katex';
+import 'katex/dist/katex.min.css';
+
+Vue.use(VueKatex, {
+  globalOptions: {
+    //... Define globally applied KaTeX options here
+  }
+});
 
 Vue.config.productionTip = false
 
