8745ef02a7a3c933f497ec75abf9d1ebefbc58e9
patiesoft <patiesoft@gmail.com>
2020-06-29 00:44:49 +0200

    Done with Classifying Critical Points

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -89,7 +89,7 @@
                         <!--Headings-->
                         <strong class="text-danger">Classifying extreme points</strong>
                         <h5 class="font-weight-bold pb-2 pt-2">MS202 </h5>
-                        <p class="font-weight-bold pb-3">Examples on functions of two variables <span v-katex="'f(x,y)'"></span></p>
+                        <p class="font-weight-bold pb-3">Examples on functions of two variables <span v-katex="'\\bf{f(x,y)}'"></span></p>
                         <h6 class="font-weight-bold pb-1 headingUnd">SECOND DERIVATIVE TEST FOR LOCAL EXTREMA </h6> 
                         <!--/Headings-->
 
@@ -115,21 +115,141 @@
                         <P>
                             The expression <span v-katex="' D = f_{xx}f_{yy} - f_{xy}^2 '"></span> is known as the <span class="font-weight-bold">discriminant</span> of <span v-katex="' f'"></span>.
                         </P> 
-                        <div class="p-2 pl-4 note note-success"> 
-                            <p class="text-left">
-                                <strong> Example (1)</strong><br>
-                                Find and classify all the critical points of the function  
-                                <span v-katex:display="'f(x, y) = x^2 + 4xy + 4x + 2y^2 - 8y + 3'"></span>
-                                <strong>Solutions:</strong>
-                                We start by evaluating the derivatives and equating them to zero to find the critical points. This leads to
-                                <span v-katex="' '"></span><br>
-                                <span v-katex:display="' '"></span>
-                                <span v-katex:display="'f_y = 4x + 4y - 8 = 0 \\text{   (1)}'"></span><br>
-                                <span v-katex:display="'f_x = 2x + 4y + 4 = 0 \\text{   (2)}'"></span><br>
-                            </p>
-                            <div v-katex="'\\begin{alignedat}{2} 10&x+ &3&y = 2 \\newline 3&x+&13&y = 4 \\end{alignedat}'"></div>
+                        <!--Example 1-->
+                        <div class="p-2 pl-4 mb-2 note note-primary text-left"> 
+                            <strong> Example (1)</strong><br>
+                            Find and classify all the critical points of the function  
+                            <div v-katex:display="'f(x, y) = x^2 + 4xy + 4x + 2y^2 - 8y + 3'"></div>
                             
+                            <span v-katex="'\\textbf{Solutions:}'"></span>
+                            We start by evaluating the derivatives and equating them to zero to find the critical points. This leads to
+                            <div v-katex:display="'\\begin{aligned} \\tag{1} f_y = 4x + 4y - 8 = 0  \\end{aligned}'"></div>
+                            <div v-katex:display="'\\begin{aligned} \\tag{2} f_x = 2x + 4y + 4 = 0  \\end{aligned}'"></div>
+                            Eliminate <span v-katex="' y '"></span> by subtracting equation <span v-katex="' \\tag{1} '"></span> from equation <span v-katex="' \\tag{2} '"></span> to get
+                            <div v-katex:display="'2x - 12 = 0,; \\Longrightarrow x = 6,'"></div>
+                            Substituting <span v-katex="' x = 6 '"></span> in <span v-katex="' \\tag{1} '"></span> gives
+                            <div v-katex:display="'2(6) + 4y + 4 = 16 + 4y = 0,; \\Longrightarrow y = -4'"></div>
+                            The point <span v-katex="' (6, -4) '"></span> is the critical point of <span v-katex="' f(x,y '"></span>
+                            To classify the critical points we evaluate the second derivatives and the discriminant D then we apply the second derivative test.
+
+                            <br>
+                            The second derivatives are
+
+                            <div v-katex:display="'f_{xx} = 2, \\;\\; f_{yy} = 4,\\;\\; \\textrm{and} \\;\\;  f_{xy} = 4'"></div>
+                            Since <span v-katex="' D < 0'"></span>, we conclude that the function has a saddle point at<span v-katex:display="' (6,-4) '"></span> 
                         </div> 
+                        <!--/Example 1-->
+                        <!--Example 2 -->
+                        <div class="p-2 pl-4 mb-2 note note-primary text-left"> 
+                            <strong> Example (2)</strong><br>
+                            Find and classify all the critical points of the function  
+                            <div v-katex:display="'f(x, y) = x^3 - 12 x y + 8 y^3'"></div>
+                            <span v-katex="'\\textbf{Solutions:}'"></span>
+                            We start by evaluating the derivatives and equating them to zero to find the critical points. This leads to
+                            <div v-katex:display="'\\begin{aligned} \\tag{1} f_x = 3x^2 - 12 y = 3(x^2 - 4y) = 0  \\end{aligned}'"></div>
+                            <div v-katex:display="'\\begin{aligned} \\tag{2} f_y = 24y^2 - 12 x = 12(2y^2 - x) = 0  \\end{aligned}'"></div>
+
+                            Thus we must solve <span v-katex="' x^2 = 4y '"></span> and <span v-katex="' x = 2y^2 '"></span>.
+                            Sustituting <span v-katex="' x = 2y^2 '"></span> in the first equation gives 
+
+                            <div v-katex:display="'(2y^2)^2 = 4(y),\\;\\;\\;\\textrm{or}\\;\\; 4y^4 - 4y =4y(y^3 - 1) = 0'"></div>
+
+                            Solving gives <span v-katex="'y = 0'"></span> or <span v-katex="'y = 1'"></span>. When <span v-katex="'y = 0'"></span>, <span v-katex="'x = 0'"></span> and when <span v-katex="'y  = 1'"></span>, <span v-katex="' x = 2'"></span>. Thus the critical points are 
+                            <div v-katex:display="'(0,0)\\;\\;\\;\\textrm{and}\\;\\;\\;(2,1)'"></div>  
+
+                            To classify the critical points we evaluate the second derivatives and the discriminant <span v-katex="'D  = f_{xx} f_{yy} - f_{xy}^2'"></span> then we apply the second derivative test.
+
+                            <br>
+                            The derivatives are
+
+                            <div v-katex:display="'f_{xx} = 6x, \\;\\; f_{yy} = 48y,\\;\\; \\textrm{and} \\;\\;  f_{xy} = -12'"></div>
+                            <span v-katex="'\\textbf{Classifying  (0,0)}: \\newline'"></span>
+
+                            At (0,0): <span v-katex="' f_{xx} = 6(0) = 0'"></span>, <span v-katex=" 'f_{yy} = 48 (0) = 0'"></span>, <span v-katex="'f_{xy} = -12'"></span>
+                            <br>
+                           Thus <span v-katex="'D = f_{xx} f_{yy} - f_{xy}^2 = (0)(0) - (-12)^2 = -144 < 0.'"></span>
+                           <br>
+                            Since <span v-katex="' D < 0'"></span>, we conclude that the function has a saddle point at <span v-katex="'(0,0)'"></span>
+                            <br>
+                            <br>
+
+                            <span v-katex="'\\textbf{Classifying  (2,1)}: \\newline'"></span>
+                            At (2,1): <span v-katex="' f_{xx} = 6(2) = 12'"></span>,<span v-katex="' f_{yy} = 48(1) = 48'"></span>, <span v-katex="' f_{xy} = -12'"></span> 
+                            <br> 
+                            Thus <span v-katex="'D = f_{xx} f_{yy} - f_{xy}^2 = (12)(48) -(-12)^2 = 432 > 0.'"></span>
+                            <br>
+                             Since <span v-katex="' D > 0 '"></span>and <span v-katex="'f_{xx} > 0'"></span> we conclude that the function has a 
+                             local minimum at <span v-katex="'(2,1)'"></span>. The minimum of the function is <div v-katex:display="'f(2, 1) = (2)^3 - 12(2)(1) + 8(1)^3 = -8'"></div>
+
+
+
+
+
+                        </div> 
+                        <!--/Example 2-->
+                        <!--Example 3-->
+                        <div class="p-2 pl-4 mb-2 note note-primary text-left"> 
+                            <strong> Example (3)</strong><br>
+                            Find and classify all the extreme values of the function 
+                           <div v-katex:display="'f(x, y) = x^3 + 3 x^2 + y^3 - 3 y^2 - 8'"></div>
+                            <span v-katex="'\\textbf{Solutions: }'"></span>
+                            Equate derivatives to zero to get critical points.
+                            <div v-katex:display="'\\begin{aligned} f_x &= 3 x^2 + 6 x = 3x(x + 2) = 0  \\end{aligned}'"></div>
+                            <div v-katex:display="'\\begin{aligned} f_y &= 3 y^2 - 6 y = 3y(y - 2) = 0 \\end{aligned}'"></div>
+
+                            From <span v-katex="'3x(x + 2) = 0'"></span>, we must have <span v-katex="'3x = 0'"></span> or <span v-katex="' x+2 = 0'"></span>. Thus
+                            <div v-katex:display="'x = 0\\;\\;\\;  \\textrm{or}\\;\\;\\; x = -2'"></div> 
+                            From <span v-katex="'3y(y - 2) = 0'"></span>, we must have <span v-katex="'3y = 0'"></span> or <span v-katex="' y - 2 = 0'"></span>. Thus
+                            <div v-katex:display="'y = 0\\;\\;\\;  \\textrm{or}\\;\\;\\; y = -2'"></div> 
+
+                            From <span v-katex="'x = 0'"></span> or <span v-katex="'x = -2'"></span> and <span v-katex="'y = 0'"></span> or <span v-katex="'y = 2'"></span>, we get the critical points
+                            <div v-katex:display="'(-2,0),\\;\\; (-2,2),\\;\\; (0,0),\\;\\; (0,2)'"></div>
+
+                            To classify the points we compute the discriminant $D$ at each critical point. First note that <span v-katex="'D = f_{xx}f_{yy}-f_{xy}^2'"></span> and 
+                            <div v-katex:display="'f_{xx} = 6x + 6,\\;\\;\\; f_{yy} = 6y - 6,\\;\\; \\textrm{and}\\;\\;\\; f_{xy} = 0'"></div>
+                            <br>
+                            <br>
+
+                            <span v-katex="'\\textbf{Classifying  (-2,0)}: \\newline'"></span>
+                            At <span v-katex="'(-2,0)'"></span>: <span v-katex="'f_{xx} = 6(-2) + 6  = -6'"></span>, <span v-katex="'f_{yy} = 6(0) - 6  = -6, f_{xy} = 0\\newline'"></span>
+                            Thus <span v-katex="'D = f_{xx} f_{yy} - f_{xy}^2 = (-6)(-6) - 0^2 = 36 > 0.'"></span>
+
+                            Since <span v-katex="'D > 0'"></span> and <span v-katex="'f_{xx} < 0'"></span>, we conclude that at <span v-katex="'(-2,0)'"></span> the function has a local 
+                            maximum. The maximum of the function is 
+                            <br> 
+                            <div v-katex:display="'f(-2, 0) = (-2)^3 + 3 (-2)^2 + (0)^3 - 3 (0)^2 - 8 = -4.'"></div>
+                            <br>
+                            <br>
+
+                            <span v-katex="'\\textbf{Classifying  (-2,2)}: \\newline'"></span>
+                            At <span v-katex="'(-2,2)'"></span>: <span v-katex="'f_{xx} = 6(-2) + 6  = -6'"></span>, <span v-katex="'f_{yy} = 6(2) - 6   = 6, f_{xy} = 0\\newline'"></span>
+                            Thus <span v-katex="'D = f_{xx} f_{yy} - f_{xy}^2 = (-6)(-6) - 0^2 = -36 < 0.'"></span>
+                            Since <span v-katex="'D < 0'"></span>, we conclude that the function has a saddle point at <span v-katex="'(-2,2)'"></span>
+                            <br>
+                            <br>
+
+                            <span v-katex="'\\textbf{Classifying  (0,0)}: \\newline'"></span>
+                            At <span v-katex="'(0,0)'"></span>: <span v-katex="'f_{xx} = 6(0) + 6  = 6'"></span>, <span v-katex="'f_{yy} = 6(0) - 6   = -6, f_{xy} = 0\\newline'"></span>
+                            Thus <span v-katex="'D = f_{xx} f_{yy} - f_{xy}^2 = (6)(-6) - 0^2 = -36 < 0.'"></span>
+                            Since <span v-katex="'D < 0'"></span>, we conclude that the function has a saddle point at <span v-katex="'(0,0)'"></span>
+                            <br>
+                            <br>
+
+                            <span v-katex="'\\textbf{Classifying  (0,2)}: \\newline'"></span>
+                            At <span v-katex="'(0,2)'"></span>: <span v-katex="'f_{xx} = 6(0) + 6  = 6'"></span>, <span v-katex="'f_{yy} = 6(2) - 6   = 6, f_{xy} = 0\\newline'"></span>
+                            Thus <span v-katex="'D = f_{xx} f_{yy} - f_{xy}^2 = (6)(6) - 0^2 = 36 > 0.'"></span>
+                            Since <span v-katex="'D > 0'"></span> and <span v-katex="'f_{xx} > 0'"></span> we conclude that at <span v-katex="'(0,2)'"></span> the function has a local minimum. The minimum of the function is 
+                            <div v-katex:display="'f(0, 2) = (0)^3 + 3 (0)^2 + (2)^3 - 3 (2)^2 - 8 = -12'"></div>
+
+
+
+
+
+
+
+                        </div>
+             
+                        <!--Example 3-->
 
                     </div>
                     <!--/Classifying Critical Points--> 
